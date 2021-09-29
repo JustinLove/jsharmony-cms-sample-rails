a6624780555d268c36f745b0f6d081ff960a1b34
Justin Love <jl@apharmony.com>
2021-09-29 11:14:49 -0500

    using cms page settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  def page
+    @page ||= load_cms_page
+  end
+  helper_method :page
+
   def load_cms_page
     path = "public/cms/content/#{params[:controller]}/#{params[:action]}.html"
     CmsPage.new(JSON.parse(File.read(path)))
--- app/controllers/random_numbers_controller.rb
+++ app/controllers/random_numbers_controller.rb
@@ -1,6 +1,5 @@
 class RandomNumbersController < ApplicationController
   def index
-    @page = load_cms_page
     @number = SecureRandom.hex(32)
   end
 end
