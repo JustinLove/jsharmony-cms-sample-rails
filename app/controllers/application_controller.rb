a1868aecce8cf006bf80d8ff25b902dc7a605c19
Justin Love <jl@apharmony.com>
2021-09-29 09:45:38 -0500

    proof of concept for standalone pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def load_cms_page
+    path = "public/cms/content/#{params[:controller]}/#{params[:action]}.html"
+    CmsPage.new(JSON.parse(File.read(path)))
+  rescue
+    CmsPage.new({})
+  end
 end
--- app/controllers/random_numbers_controller.rb
+++ app/controllers/random_numbers_controller.rb
@@ -1,5 +1,6 @@
 class RandomNumbersController < ApplicationController
   def index
+    @page = load_cms_page
     @number = SecureRandom.hex(32)
   end
 end
